fd4a1d001c1ebbe41ce3e506d13dcba1559347ed
Tao Zou <thomasdic2000@gmail.com>
2017-09-07 14:31:47 +0000

    training/testing scripts added: run_demo_solar***.sh, train_solarPanel_RFCN.sh

---
+++ run_demo_solar_drawDetection.sh
@@ -0,0 +1,3 @@
+#python ./tools/demo_solar.py --cfg experiments/cfgs/rfcn_end2end_SP.yml --input data/demo_solar/
+#python tools/demo_solar.py --cfg experiments/cfgs/rfcn_end2end_SP.yml --input ../addressdata-cache/nearmap_images/test/
+python tools/demo_solar.py --cfg experiments/cfgs/rfcn_end2end_SP.yml --input solar_test/test/
---
+++ train_solarPanel_RFCN.sh
@@ -0,0 +1,1 @@
+./tools/train_net.py --gpu 0 --solver models/solar_panel/ResNet-101/rfcn_end2end/solver.prototxt --weights data/imagenet_models/ResNet-101-model.caffemodel --imdb solar_panel_train --iters 40000 --cfg experiments/cfgs/rfcn_end2end.yml
